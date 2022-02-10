f80aaca8d097a84b01dd2bd468ec90904e6c138d
pinkeraw <anh.vdq@gmail.com>
2022-02-10 14:49:41 +0700

    Fix enclose dynamic column in double quote

--- src/main/java/dev/keter/model/DynamicColumn.java
+++ src/main/java/dev/keter/model/DynamicColumn.java
@@ -47,6 +47,6 @@ public class DynamicColumn {
 
     @Override
     public String toString() {
-        return columnName + " " + dataType;
+        return "\"" + columnName + "\" " + dataType;
     }
 }
